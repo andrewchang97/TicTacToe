baa9060970cd26fdf37777f93087c099a9bc99f5
Andrew <andrewhoonchang97@gmail.com>
2023-08-23 22:33:23 -0700

    Adding html, css, and js file of my tic tac toe project, not much functionality at all but i got some effects going

---
+++ main.js
@@ -0,0 +1,74 @@
+const colorIn = `rgba(128, 241, 214, 0.3)`;
+const colorOut = '#5a7aa9';
+//box 1
+const box1 = document.querySelector('.grid1');
+box1.addEventListener('mouseover', () => {
+    box1.style.backgroundColor = colorIn;
+});
+box1.addEventListener('mouseout', () => {
+    box1.style.backgroundColor = colorOut;
+});
+//box 2
+const box2 = document.querySelector('.grid2');
+box2.addEventListener('mouseover', () => {
+    box2.style.backgroundColor = colorIn;
+});
+box2.addEventListener('mouseout', () => {
+    box2.style.backgroundColor = colorOut;
+});
+//box 3
+const box3 = document.querySelector('.grid3');
+box3.addEventListener('mouseover', () => {
+    box3.style.backgroundColor = colorIn;
+});
+box3.addEventListener('mouseout', () => {
+    box3.style.backgroundColor = colorOut;
+});
+//box 4
+const box4 = document.querySelector('.grid4');
+box4.addEventListener('mouseover', () => {
+    box4.style.backgroundColor = colorIn;
+});
+box4.addEventListener('mouseout', () => {
+    box4.style.backgroundColor = colorOut;
+});
+//box 5
+const box5 = document.querySelector('.grid5');
+box5.addEventListener('mouseover', () => {
+    box5.style.backgroundColor = colorIn;
+});
+box5.addEventListener('mouseout', () => {
+    box5.style.backgroundColor = colorOut;
+});
+//box 6
+const box6 = document.querySelector('.grid6');
+box6.addEventListener('mouseover', () => {
+    box6.style.backgroundColor = colorIn;
+});
+box6.addEventListener('mouseout', () => {
+    box6.style.backgroundColor = colorOut;
+});
+//box 7
+const box7 = document.querySelector('.grid7');
+box7.addEventListener('mouseover', () => {
+    box7.style.backgroundColor = colorIn;
+});
+box7.addEventListener('mouseout', () => {
+    box7.style.backgroundColor = colorOut;
+});
+//box 8
+const box8 = document.querySelector('.grid8');
+box8.addEventListener('mouseover', () => {
+    box8.style.backgroundColor = colorIn;
+});
+box8.addEventListener('mouseout', () => {
+    box8.style.backgroundColor = colorOut;
+});
+//box 9
+const box9 = document.querySelector('.grid9');
+box9.addEventListener('mouseover', () => {
+    box9.style.backgroundColor = colorIn;
+});
+box9.addEventListener('mouseout', () => {
+    box9.style.backgroundColor = colorOut;
+});

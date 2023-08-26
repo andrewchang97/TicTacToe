cda18fedd1099a8806ddbbea26a91196dc0b5fa4
Andrew <andrewhoonchang97@gmail.com>
2023-08-26 12:47:21 -0700

    Adding 'finished' tictactoe project, but the final space will not register a click no matter what I do, I even asked chatGPT and we could not find any errors. Going to move on to save time but I'm planning on coming back in a few days and maybe I'll find what's wrong

--- main.js
+++ main.js
@@ -1,5 +1,82 @@
 const colorIn = `rgba(128, 241, 214, 0.3)`;
 const colorOut = '#5a7aa9';
+const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector("#statusText");
+const restartButton = document.querySelector(".reset");
+const winConditions = [
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [4, 5, 6],
+    [7, 8, 9],
+    [2, 5, 8],
+    [3, 6, 9],
+    [3, 5, 7]
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X"
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked));
+    restartButton.addEventListener("click", restartGame);
+    statusText.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+    if (options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+function updateCell(cell, index){
+    options[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
+}
+function checkWinner(){
+    let roundWon = false;
+    for (let i = 0; i < winConditions.length; i++){
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if (cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if (roundWon){
+        statusText.textContent = `${currentPlayer} wins!`
+        running = false;
+    }
+    else if(!options.includes("")){
+        statusText.textContent = `Draw!`
+        running = false;
+    }
+    else {
+        changePlayer();
+    }
+}
+function restartGame(){
+    currentPlayer = "X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+    running = true;
+}
 //box 1
 const box1 = document.querySelector('.grid1');
 box1.addEventListener('mouseover', () => {
